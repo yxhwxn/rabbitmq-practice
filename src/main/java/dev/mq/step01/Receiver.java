83155e0cc32ff0083041f2c926b80b6e1620b770
yxhwxn <officeyh9819@gmail.com>
2025-04-09 10:18:42 +0900

    step01: DeliverCallback을 활용한 메시지 소비 콜백 함수 정의 추가

--- src/main/java/dev/mq/step01/Receiver.java
+++ src/main/java/dev/mq/step01/Receiver.java
@@ -2,39 +2,40 @@ package dev.mq.step01;
 
 import com.rabbitmq.client.*;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 /**
  * Receiver - 메시지 소비자(Consumer)
  * Sender - 메시지 발행자(Publisher)
  *
- * Recevier는 RabbitMQ와 연결, 메시지를 전송하는 처리 수행
- * Sencder는 메시지 큐로부터 퍼블리셔(Receiver가 적재한 메시지를 소비하는 처리 수행
+ * Receiver는 RabbitMQ와 연결, 메시지를 전송하는 처리 수행
+ * Sender는 메시지 큐로부터 퍼블리셔(Receiver)가 적재한 메시지를 소비하는 처리 수행
  */
 public class Receiver {
 
-    private final static String QUEUE_NAME = "hello-queue";
+    private final static String QUEUE_NAME = "hello";
 
-    public static void main(String[] argv) throws Exception {
-        // ConnectionFactory: RabbitMQ 서버 프로세스로의 연결 수행
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // RabbitMQ 서버 프로세스로의 연결 수행
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        factory.setUsername("guest");
-        factory.setPassword("guest");
 
-        // 연결 및 채널 생성
+        // RabbitMQ 서버(5672)와 커넥션 연결 수행
         Connection connection = factory.newConnection();
+        // 통신 채널 생성
         Channel channel = connection.createChannel();
 
+        // 큐 생성
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        System.out.println(" [Receiver] 메시지 기다리는 중.");
 
-        // 메시지 수신 처리 로직
+        // 메시지를 소비하기 위한 콜백 함수 정의
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
-            System.out.println(" [x] Received '" + message + "'");
+            System.out.println(" [Receiver] 메시지 받음: " + message);
         };
 
-        // 메시지 소비 시작
-        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
-        });
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
     }
 }
--- src/main/java/dev/mq/step01/Sender.java
+++ src/main/java/dev/mq/step01/Sender.java
@@ -4,35 +4,38 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
 /**
  * Receiver - 메시지 소비자(Consumer)
  * Sender - 메시지 발행자(Publisher)
  *
- * Recevier는 RabbitMQ와 연결, 메시지를 전송하는 처리 수행
- * Sencder는 메시지 큐로부터 퍼블리셔(Receiver가 적재한 메시지를 소비하는 처리 수행
+ * Receiver는 RabbitMQ와 연결, 메시지를 전송하는 처리 수행
+ * Sender는 메시지 큐로부터 퍼블리셔(Receiver)가 적재한 메시지를 소비하는 처리 수행
  */
 public class Sender {
 
-    private final static String QUEUE_NAME = "hello-queue";
+    private final static String QUEUE_NAME = "hello";
 
-    public static void main(String[] args) throws Exception {
-        // ConnectionFactory: RabbitMQ 서버 프로세스로의 연결 수행
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // RabbitMQ 서버 프로세스로의 연결 수행
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        factory.setUsername("guest");
-        factory.setPassword("guest");
 
-        // 연결 및 채널 생성
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
+        // RabbitMQ 서버(5672)와 커넥션 연결 수행
+        Connection connection = factory.newConnection();
+        // 통신 채널 생성
+        Channel channel = connection.createChannel();
 
-            // 큐 선언
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello RabbitMQ!";
+        // 큐 생성
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello world!";
 
-            // 메시지 전송
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-            System.out.println(" [x] Sent '" + message + "'");
-        }
+        // 메시지 발행
+        channel
+                .basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+        System.out.println(" [Publisher] Sent " + message);
     }
 }
